ad33c7c99b4c9a6a355854894ff9065247b7d0f1
Palak Maheria <palak.maheria@hotmail.com>
2014-04-11 15:34:20 +0530

    First commit WPF 3D animation pure xaml
    WPF 3D animation pure xaml

---
+++ TranslateTransformConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf3DAnimation
+{
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class TranslateTransformConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return 0;
+            }
+
+            var seconds = System.Convert.ToInt32(value);
+            var miliseconds = System.Convert.ToInt32((System.Convert.ToDouble(value, CultureInfo.InstalledUICulture) % 1) * 100);
+            var newValue = new System.Windows.Duration(new TimeSpan(0, 0, seconds, miliseconds)); //// System.Convert.ToDouble(value);
+            return newValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+}
